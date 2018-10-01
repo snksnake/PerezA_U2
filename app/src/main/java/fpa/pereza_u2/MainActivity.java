c15dd7648cf6428aa70f464b2996116ff1fc739d
Alberto <snk_snake@hotmail.com>
2018-10-01 21:07:57 +0200

    Tabs added

--- app/src/main/java/fpa/pereza_u2/MainActivity.java
+++ app/src/main/java/fpa/pereza_u2/MainActivity.java
@@ -1,13 +1,29 @@
 package fpa.pereza_u2;
 
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentTabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabHost= findViewById(android.R.id.tabhost);
+        tabHost.setup(this,
+                getSupportFragmentManager(),android.R.id.tabcontent);
+
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("Asteroides"),
+                Tab1.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("Constraint"),
+                Tab2.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Lengüeta 3"),
+                Tab3.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab4").setIndicator("Lengüeta 4"),
+                Tab4.class, null);
+
     }
 }
---
+++ app/src/main/java/fpa/pereza_u2/Tab4.java
@@ -0,0 +1,21 @@
+package fpa.pereza_u2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab4 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab4_view, container, false);
+    }
+}
